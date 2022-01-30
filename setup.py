e76511d955fd9df912e9aad528a3459db0c015e9
ksmin23 <sonagy23@gmail.com>
2022-01-30 17:13:51 +0900

    migrating to AWS CDK v2

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ setuptools.setup(
     packages=setuptools.find_packages(where="image_insights"),
 
     install_requires=[
-        "aws-cdk.core",
+        "aws-cdk-lib",
+        "constructs"
     ],
 
     python_requires=">=3.6",
