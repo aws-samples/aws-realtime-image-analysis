cc7da8d07aed19afaaf4f28c0c1a7c130612f988
ksmin23 <sonagy23@gmail.com>
2023-02-03 15:43:54 +0900

    create a new VPC instead of using an existing one

--- image_insights/vpc.py
+++ image_insights/vpc.py
@@ -22,38 +22,37 @@ class ImageInsightsVpcStack(Stack):
     # for example,
     # cdk -c vpc_name=your-existing-vpc syth
     #
-    vpc_name = self.node.try_get_context('vpc_name')
-    vpc = aws_ec2.Vpc.from_lookup(self, 'ExistingVPC',
-      is_default=True,
-      vpc_name=vpc_name
-    )
-
-    # vpc = aws_ec2.Vpc(self, "ImageInsightsVPC",
-    #   cidr="10.31.0.0/21",
-    #   max_azs=2,
-    #   # subnet_configuration=[{
-    #   #     "cidrMask": 24,
-    #   #     "name": "Public",
-    #   #     "subnetType": aws_ec2.SubnetType.PUBLIC,
-    #   #   },
-    #   #   {
-    #   #     "cidrMask": 24,
-    #   #     "name": "Private",
-    #   #     "subnetType": aws_ec2.SubnetType.PRIVATE_WITH_NAT
-    #   #   },
-    #   #   {
-    #   #     "cidrMask": 28,
-    #   #     "name": "Isolated",
-    #   #     "subnetType": aws_ec2.SubnetType.PRIVATE_ISOLATED,
-    #   #     "reserved": True
-    #   #   }
-    #   # ],
-    #   gateway_endpoints={
-    #     "S3": aws_ec2.GatewayVpcEndpointOptions(
-    #       service=aws_ec2.GatewayVpcEndpointAwsService.S3
-    #     )
-    #   }
+    # vpc_name = self.node.try_get_context('vpc_name')
+    # vpc = aws_ec2.Vpc.from_lookup(self, 'ExistingVPC',
+    #   is_default=True,
+    #   vpc_name=vpc_name
     # )
+
+    vpc = aws_ec2.Vpc(self, "ImageInsightsVPC",
+      ip_addresses=aws_ec2.IpAddresses.cidr("10.0.0.0/21"),
+      max_azs=3,
+
+      # 'subnetConfiguration' specifies the "subnet groups" to create.
+      # Every subnet group will have a subnet for each AZ, so this
+      # configuration will create `2 groups × 3 AZs = 6` subnets.
+      subnet_configuration=[
+        {
+          "cidrMask": 24,
+          "name": "Public",
+          "subnetType": aws_ec2.SubnetType.PUBLIC,
+        },
+        {
+          "cidrMask": 24,
+          "name": "Private",
+          "subnetType": aws_ec2.SubnetType.PRIVATE_WITH_EGRESS
+        }
+      ],
+      gateway_endpoints={
+        "S3": aws_ec2.GatewayVpcEndpointOptions(
+          service=aws_ec2.GatewayVpcEndpointAwsService.S3
+        )
+      }
+    )
 
     self.vpc = vpc
 
